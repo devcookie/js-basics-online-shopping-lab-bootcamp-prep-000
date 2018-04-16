c4eab0154e0451460ed98478027d996776b3a90e
Catherine Lee <lee.catherine77@gmail.com>
2018-04-16 20:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  getCart()
+  console.log(getCart())
   for(var i = 0; i < cart.length; i++) {
     if(getCart()[i].itemName === item) {
       getCart().slice(i)

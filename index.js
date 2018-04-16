e6073cf933400b36303d712089d1260c56254716
Catherine Lee <lee.catherine77@gmail.com>
2018-04-16 21:17:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  cart = `${cart}`
+  cart.push(`${cart}`)
   for(var i = 0; i < cart.length; i++) {
     if(cart[i].itemName === item) {
       cart.splice(i, 1)

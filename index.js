f37eaefff050f744675363631f17f59343e23440
Catherine Lee <lee.catherine77@gmail.com>
2018-04-16 20:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push(Object.assign({}, {itemName:item, itemPrice:Math.random()* (100 - 1) + 1}))
+ cart.push(Object.assign({}, {itemName:item, itemPrice:Math.floor(Math.random()* (100 - 1) + 1)}))
  return `${item} has been added to your cart.`
 }
 
@@ -25,18 +25,30 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
     var longCart = []
-    for(var i = 0; i < cart.length; i++) {
-      
+    for(var i = 0; i < cart.length-1; i++) {
+      longCart.push(' '+cart[i].itemName+' at $'+cart[i].itemPrice)
     }
+    return `In your cart, you have${longCart}, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
   }
 }
 
 function total() {
   // write your code here
+  var sum = 0
+  var sumArray = []
+  for(var i = 0; i < cart.length; i++) {
+    sum += cart[i].itemPrice
+  }
+  return sum
 }
 
 function removeFromCart(item) {
   // write your code here
+  for(var i = 0; i < cart.length; i++) {
+    if(getCart()[i].itemName === item) {
+      getCart().slice(i, 1)
+    }
+  } return getCart()
 }
 
 function placeOrder(cardNumber) {

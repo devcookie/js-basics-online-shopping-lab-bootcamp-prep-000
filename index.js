8c7f963e8bd3e9b7d6b412473b157284ec04e499
Catherine Lee <lee.catherine77@gmail.com>
2018-04-16 21:30:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for(var i = 0; i < cart.length; i++) {
-    if(cart[i].itemName === item) {
+    if(cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1)
       return cart
     } else {

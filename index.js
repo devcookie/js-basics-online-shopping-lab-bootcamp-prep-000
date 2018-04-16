9fcd445ccb699c4e5a84a4fb4be7c6f3c515fd56
Catherine Lee <lee.catherine77@gmail.com>
2018-04-16 21:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ function removeFromCart(item) {
 
     } else {
       return 'That item is not in your cart.'
-    }
-  } return cart
+    } return cart
+  }
 }
 
 function placeOrder(cardNumber) {
